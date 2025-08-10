ef5167dda5a09ecfe71e5e253ae82ff717049c67
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-08-10 10:57:37 -0400

    Add example messages.

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -1,3 +1,70 @@
+"use client";
+
+const myUUID = crypto.randomUUID();
+const theirUUID = crypto.randomUUID();
+const now = new Date();
+
+const messages = [
+  {
+    id: crypto.randomUUID(),
+    sender: myUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 8 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: myUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 7 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: theirUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 6 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: theirUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 5 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: theirUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 4 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: myUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 3 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: theirUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 2 * 1000),
+  },
+  {
+    id: crypto.randomUUID(),
+    sender: myUUID,
+    text: "Hey what is uup",
+    time: new Date(now.getTime() - 1 * 1000),
+  },
+] as const;
+
+const MessageRow = ({ message }: { message: (typeof messages)[number] }) => {
+  return <div>{message.text}</div>;
+};
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div>
+      {messages.map((message) => (
+        <MessageRow message={message} key={message.id} />
+      ))}
+    </div>
+  );
 }
