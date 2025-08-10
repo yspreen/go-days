1f57954be2657e0e0e942f3d62995c20d15674f3
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-08-10 19:35:59 -0400

    Add suspense boundary.

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -2,8 +2,16 @@
 
 import { RoomComponent } from "@/components/room";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Home() {
+  return (
+    <Suspense>
+      <Loaded />
+    </Suspense>
+  );
+}
+function Loaded() {
   const roomId = useSearchParams().get("roomId") || undefined;
 
   return <RoomComponent roomId={roomId} />;
