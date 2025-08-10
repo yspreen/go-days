40e80561ac20bdd4bd338286d971a6bf9b1ace15
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-08-10 12:37:57 -0400

    Add room id support.

--- react/next.config.ts
+++ react/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
+  redirects: async () => [
+    {
+      source: "/room/:roomId",
+      destination: "/?roomId=:roomId",
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
---
+++ react/src/components/room.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+interface Message {
+  id: string;
+  sender: string;
+  text: string;
+  time: Date;
+}
+
+const MessageRow = ({
+  message,
+  myUUID,
+}: {
+  message: Message;
+  myUUID: string;
+}) => {
+  const mine = message.sender === myUUID;
+  const seed = parseInt(message.sender.slice(0, 8), 16) / 0xffffffff;
+  return (
+    <div data-mine={mine} className="flex data-[mine=true]:justify-end">
+      <div
+        className="max-w-3xl m-2 p-4 rounded-lg"
+        style={{ backgroundColor: `hsl(${~~(365 * seed)},80%,80%)` }}
+      >
+        {message.text}
+      </div>
+    </div>
+  );
+};
+
+export const RoomComponent = ({ roomId = "" }) => {
+  const theirUUID = useRef(crypto.randomUUID());
+  const nowRef = useRef(new Date());
+
+  const myUUID = useRef(crypto.randomUUID());
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        id: crypto.randomUUID(),
+        sender: myUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 8 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: myUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 7 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: theirUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 6 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: theirUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 5 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: theirUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 4 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: myUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 3 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: theirUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 2 * 1000),
+      },
+      {
+        id: crypto.randomUUID(),
+        sender: myUUID.current!,
+        text: "Hey what is uup",
+        time: new Date(nowRef.current!.getTime() - 1 * 1000),
+      },
+    ]);
+    setTimeout(() => {
+      setMessages((old) => [
+        ...old,
+        {
+          id: crypto.randomUUID(),
+          sender: theirUUID.current!,
+          text: "Hey what is uup",
+          time: nowRef.current!,
+        },
+      ]);
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      {!!roomId && <div>Room: {roomId}</div>}
+      <div>
+        {messages.map((message) => (
+          <MessageRow
+            myUUID={myUUID.current!}
+            message={message}
+            key={message.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
